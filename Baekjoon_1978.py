b232fb2623115aa1e0ed25cb388886ad3b61a1b9
hwangchahae <149961402+hwangchahae@users.noreply.github.com>
2025-05-02 00:01:25 +0900

    Update Baekjoon_1978.py

--- Baekjoon_1978.py
+++ Baekjoon_1978.py
@@ -4,10 +4,9 @@ result = []
 
 for num in nums:
     if num != 1:
-        for i in range(2, num+1):
-            for j in range(2, int(i**0.5)+1):
-                if i%j == 0:
+        for i in range(2, int(num**0.5)+1):
+            if num%i == 0:
                     break
-            else :
-                result.append(num)
-print(len(result))            
+        else :
+            result.append(num)
+print(len(result))                         
