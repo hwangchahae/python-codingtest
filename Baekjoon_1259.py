c453c11fc8311d6909ef6ed5e574c0e2658c3f00
hwangchahae <149961402+hwangchahae@users.noreply.github.com>
2025-05-02 00:03:39 +0900

    Update Baekjoon_1259.py

--- Baekjoon_1259.py
+++ Baekjoon_1259.py
@@ -2,9 +2,10 @@ while(True):
     word = input()
     if word == '0':
         break
-    for i in range(1, len(word)//2 + 1):
-        if word[i] != word[-i]:
+    for i in range(0, len(word)//2):
+        if word[i] != word[-(i+1)]:
             print('no')
             break
     else :
         print('yes')
+     
