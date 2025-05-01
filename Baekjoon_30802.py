945f6120cebdd6464d86a84bdc1a71eb58d71808
hwangchahae <149961402+hwangchahae@users.noreply.github.com>
2025-05-01 23:43:38 +0900

    Create Baekjoon_30802.py

---
+++ Baekjoon_30802.py
@@ -0,0 +1,16 @@
+N = int(input())
+sizes = list(map(int, input().split()))
+T, P = map(int, input().split())
+
+total_sum = 0
+for size in sizes:
+    t = size // T + 1
+    if size % T == 0 :
+        t -= 1
+    total_sum += t 
+print(total_sum)
+
+if N % P != 0:
+    print(N//P, N % P)
+else :
+    print(N//P)
