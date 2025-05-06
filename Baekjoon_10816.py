b1e7bd66c592ee2fa14f181effc6c92792e98f14
hwangchahae <149961402+hwangchahae@users.noreply.github.com>
2025-05-07 00:33:47 +0900

    Update Baekjoon_10816.py

--- Baekjoon_10816.py
+++ Baekjoon_10816.py
@@ -1,15 +1,12 @@
 import sys
-card_dict = {}
+from collections import Counter
+
 N = int(sys.stdin.readline())
-for card in list(map(int, sys.stdin.readline().split())):
-    if card not in card_dict:
-        card_dict[card] = 1
-    else :
-        card_dict[card] += 1 
+cards = list(map(int, sys.stdin.readline().split()))
+card_counter = Counter(cards) 
 
 M = int(sys.stdin.readline())
-for n in list(map(int, sys.stdin.readline().split())):
-    if n in card_dict:
-        print(card_dict[n], end=' ')
-    else :
-        print(0, end=' ')
+targets = list(map(int, sys.stdin.readline().split()))
+
+for n in targets:
+    print(card_counter[n], end=' ')
