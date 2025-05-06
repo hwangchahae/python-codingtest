12f57c67135876fdbc50e813978bbc383e79b334
hwangchahae <149961402+hwangchahae@users.noreply.github.com>
2025-05-07 01:37:13 +0900

    Update Baekjoon_10828.py

--- Baekjoon_10828.py
+++ Baekjoon_10828.py
@@ -5,13 +5,13 @@ my_stack = []
 for _ in range(N):
     command = sys.stdin.readline().split()
     if command[0] == 'push':
-        stack.append(int(command[1]))
+        my_stack.append(int(command[1]))
     elif command[0] == 'pop':
-        print(stack.pop() if stack else -1)
+        print(my_stack.pop() if my_stack else -1)
     elif command[0] == 'size':
-        print(len(stack))
+        print(len(my_stack))
     elif command[0] == 'empty':
-        print(0 if stack else 1)
+        print(0 if my_stack else 1)
     elif command[0] == 'top':
-        print(stack[-1] if stack else -1)
+        print(my_stack[-1] if my_stack else -1)
     
