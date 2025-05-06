42154bd2bd173017b847790127d98acb2fdcb950
hwangchahae <149961402+hwangchahae@users.noreply.github.com>
2025-05-07 00:12:02 +0900

    Update Baekjoon_2164.py

--- Baekjoon_2164.py
+++ Baekjoon_2164.py
@@ -1,13 +1,13 @@
 import sys
-import queue
+from collections import deque
 
-card_queue = queue.Queue()
+card_deque = deque()
 N = int(sys.stdin.readline())
 
 for i in range(1, N+1):
-    card_queue.put(i)
+    card_deque.append(i)
 
-while card_queue.qsize() != 1:
-    card_queue.get()
-    card_queue.put(card_queue.get())
-print(card_queue.get())
+while len(card_deque) != 1:
+    card_deque.popleft()
+    card_deque.append(card_deque.popleft())
+print(card_deque.pop())
